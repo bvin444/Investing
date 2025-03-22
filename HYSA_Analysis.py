ef4d40a57416a169be3dc9250f159eacce0d8156
Test <benjaminvincent1996@gmail.com>
2025-03-22 12:35:11 -0700

    Adding Analysis Features

--- HYSA_Analysis.py
+++ HYSA_Analysis.py
@@ -4,7 +4,7 @@ from typing import ClassVar
 from typing import Dict
 class HYSAs:
     contribution_Dictionary : ClassVar[Dict] = {'daily': 365, 'monthly': 12, 'annually': 1}
-    x_Frame : ClassVar[int] = 500
+    x_Frame : ClassVar[int] = 600
     y_Frame : ClassVar[int] = 200
     def __init__(self):
         self.main()
@@ -16,9 +16,12 @@ class HYSAs:
             event, values = self.window.read()
             if event in (sg.WIN_CLOSED, "EXIT"): break
             elif event == "SUBMIT":
-                if self.input_Validation("PRINCIPAL", "APY", "CONTRIBUTION_AMOUNT", values = values): continue # break out of current iteration, and begin anew
+                if self.input_Validation("PRINCIPAL", "APY", "CONTRIBUTION_AMOUNT", "MONTHS", values = values): continue # break out of current iteration, and begin anew
                 self.Calculation(values)
-                self.window["OUTPUT"].update(f"{round((self.Total_OUTPUT - self.Hold), 2)}")
+                self.window["OUTPUT_TOTAL"].update(f"{round((self.Total_OUTPUT - self.Hold), 2)}")
+                self.window["FIRST_MONTH"].update(f"{(self.month_Array[0])}")
+                self.window["LAST_MONTH"].update(f"{(self.month_Array[len(self.month_Array) - 1])}")
+                self.window["DIFFERENCE"].update(f"{round(self.month_Array[len(self.month_Array) - 1] - self.month_Array[0], 2)}")
         self.window.close()
 
     def create_main_window(self):
@@ -27,9 +30,12 @@ class HYSAs:
             [
                 [sg.Text("Please enter your principal: "), sg.Input("", key = 'PRINCIPAL')],
                 [sg.Text("Please specify you Annual Percentage Yield: "), sg.Input("", key = "APY")],
+                [sg.Text("How many months would you like to evaluate this over?"), sg.Input("", key = "MONTHS")],
                 [sg.Text("Please specify you how regularly you contribute): "), sg.Combo(list(HYSAs.contribution_Dictionary), enable_events = True, key = "CONTRIBUTION")],
                 [sg.Text("Please enter how much you are contributingl: "), sg.Input("", key = 'CONTRIBUTION_AMOUNT')],
-                [sg.Text("You should expect to make this much over the year: "), sg.Input("", key = "OUTPUT")],
+                [sg.Text("You should expect to make this much over the year: "), sg.Input("", size = (10, 10), key = "OUTPUT_TOTAL")],
+                [sg.Text("Your first month you made: "), sg.Input("", size = (10, 10), key = "FIRST_MONTH"), sg.Text("Your last month you made: "), 
+                sg.Input("", size = (10, 10), key = "LAST_MONTH"), sg.Text("Difference: "), sg.Input("", size = (10, 10), key = "DIFFERENCE")],
                 [sg.Button("Submit", key = "SUBMIT"), sg.Button("Exit", key = "EXIT")]
             ], size = (HYSAs.x_Frame, HYSAs.y_Frame))
         layout = [[HYSA_Frame]]
@@ -37,14 +43,18 @@ class HYSAs:
         return window
     
     def Calculation(self, values):
-        # TODO: create code to perform HYSA analysis
         Prin = self.Numerical_Dictionary["PRINCIPAL"]
         self.Hold = Prin
         APY = self.Numerical_Dictionary["APY"] / 100
+        # for 
+        self.month_Array = []
         Contribution = self.Numerical_Dictionary["CONTRIBUTION_AMOUNT"]
-        for i in range(1, 13): # should be fine, as value is already an int HYSAs.contribution_Dictionary[values["CONTRIBUTION"]+1]
-            Prin = Prin + ((Prin*APY))/ 12
-        self.Total_OUTPUT = Prin # hold it in an array and then plot??? This way, I can see how much I made each month.
+        for i in range(1, int(values["MONTHS"]) + 1): # should be fine, as value is already an int HYSAs.contribution_Dictionary[values["CONTRIBUTION"]+1]
+            month_New_Value = ((Prin*APY))/ 12 # how much money I got from the month
+            Prin = Prin + month_New_Value
+            self.month_Array.append(round(month_New_Value, 2))
+
+        self.Total_OUTPUT = Prin
 
     def input_Validation(self, *args, values):
 
