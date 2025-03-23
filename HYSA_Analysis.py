9d3ca5c40528407d3c66d940946ce486bbe398e9
Test <benjaminvincent1996@gmail.com>
2025-03-22 21:49:53 -0700

    Updating HYSA Analysis Tool

--- HYSA_Analysis.py
+++ HYSA_Analysis.py
@@ -1,5 +1,6 @@
 # Code to analyze HYSAs
 # assuming daily compounding
+
 import PySimpleGUI as sg
 from typing import ClassVar
 from typing import Dict
@@ -8,22 +9,11 @@ class HYSAs:
 
     # Class Variables
     contribution_Dictionary : ClassVar[Dict] = {'daily': 365, 'monthly': 12, 'annually': 1}
-    month_days : ClassVar[Dict] = {
-        1 : 31,
-        2 : 28,
-        3 : 31,
-        4 : 30,
-        5: 31,
-        6 : 30,
-        7 : 31,
-        8 : 31,
-        9 : 30,
-        10 : 31,
-        11 : 30,
-        12 : 31
-    }
+    month_days : ClassVar[int] = [31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
 
-    x_Frame : ClassVar[int] = 600
+
+
+    x_Frame : ClassVar[int] = 750
     y_Frame : ClassVar[int] = 200
 
     def __init__(self):
@@ -36,12 +26,15 @@ class HYSAs:
             event, values = self.window.read()
             if event in (sg.WIN_CLOSED, "EXIT"): break
             elif event == "SUBMIT":
-                if self.input_Validation("PRINCIPAL", "APY", "CONTRIBUTION_AMOUNT", "MONTHS", values = values): continue # break out of current iteration, and begin anew
+                if self.input_Validation("PRINCIPAL", "APY", "CONTRIBUTION_AMOUNT", "YEARS", values = values): continue # break out of current iteration, and begin anew
                 self.Calculation(values)
                 self.window["OUTPUT_TOTAL"].update(f"{round((self.Total_OUTPUT), 2)}")
-                self.window["FIRST_MONTH"].update(f"{(self.month_Array[0])}")
-                self.window["LAST_MONTH"].update(f"{(self.month_Array[len(self.month_Array) - 1])}")
-                self.window["DIFFERENCE"].update(f"{round(self.month_Array[len(self.month_Array) - 1] - self.month_Array[0], 2)}")
+                self.window["FIRST_MONTH"].update(f"{round((self.day_Array[0])*31, 2)}")
+                self.window["LAST_MONTH"].update(f"{round((self.day_Array[len(self.day_Array) - 1]*31), 2)}")
+                self.window["DIFFERENCE"].update(f"{round((self.day_Array[len(self.day_Array) - 1]*31) - (self.day_Array[0]*31), 2)}")
+                self.window["BALANCE_TOTAL"].update(f"{round(self.Total_Balance, 2)}")
+                self.window["PERSONAL_CONTRIBUTIONS_TOTAL"].update(f"{round(self.Index * self.Numerical_Dictionary["CONTRIBUTION_AMOUNT"], 2)}")
+
             elif event == "RESET":
                 self.Reset()
         self.window.close()
@@ -52,10 +45,11 @@ class HYSAs:
             [
                 [sg.Text("Please enter your principal: "), sg.Input("", key = 'PRINCIPAL')],
                 [sg.Text("Please specify you Annual Percentage Yield: "), sg.Input("", key = "APY")],
-                [sg.Text("How many years would you like to evaluate this over?"), sg.Input("", key = "MONTHS")],
+                [sg.Text("How many years would you like to evaluate this over?"), sg.Input("", key = "YEARS")],
                 [sg.Text("Please specify you how regularly you contribute): "), sg.Combo(list(HYSAs.contribution_Dictionary), enable_events = True, key = "CONTRIBUTION")],
                 [sg.Text("Please enter how much you are contributingl: "), sg.Input("", key = 'CONTRIBUTION_AMOUNT')],
-                [sg.Text("You should expect to make this much over the year (interest): "), sg.Input("", size = (10, 10), key = "OUTPUT_TOTAL")],
+                [sg.Text("You should expect to make this much over the year (interest): "), sg.Input("", size = (10, 10), key = "OUTPUT_TOTAL"), sg.Text("Your Balance Total: "), sg.Input("", size = (10, 10), 
+                key = "BALANCE_TOTAL"), sg.Text("Total Contribution"), sg.Input("", key = "PERSONAL_CONTRIBUTIONS_TOTAL")],
                 [sg.Text("Your first month you made: "), sg.Input("", size = (10, 10), key = "FIRST_MONTH"), sg.Text("Your last month you made: "), 
                 sg.Input("", size = (10, 10), key = "LAST_MONTH"), sg.Text("Difference: "), sg.Input("", size = (10, 10), key = "DIFFERENCE")],
                 [sg.Button("Submit", key = "SUBMIT"), sg.Button("Reset", key = "RESET"), sg.Button("Exit", key = "EXIT")]
@@ -64,41 +58,41 @@ class HYSAs:
         window = sg.Window("HYSA Analysis", layout, resizable = True)
         return window
     
-    def Calculation(self, values):
+    def Calculation(self, values): # add contribution start from there.
         Prin = self.Numerical_Dictionary["PRINCIPAL"]
-        contribution = self.Numerical_Dictionary["CONTRIBUTION_AMOUNT"]
         self.Hold = Prin
         APY = self.Numerical_Dictionary["APY"] / 100
-        self.month_Index = 1
-        self.month_Array = []
-        for i in range(1, int(values["MONTHS"]) + 1): 
+        self.day_Array = []
+        day = 0
+        self.Index = 0
+        daily_Rate = ((1+APY)**(1/365) - 1)
+        print(daily_Rate)
+
+        for i in range(1, (int(values["YEARS"]) * 365) + 1): # 1 - 365
+            print(i)
+            day = day + 1
 
             if values["CONTRIBUTION"] == 'daily': 
-                Prin = self.daily(Prin, contribution, self.month_Index)
-                self.month_Index = self.month_Index + 1
-                if self.month_Index == 13:
-                    self.month_Index = 1
+                Prin = Prin + self.Numerical_Dictionary["CONTRIBUTION_AMOUNT"]
 
-            elif values["CONTRIBUTION"] == 'monthly':
+            elif values["CONTRIBUTION"] == 'monthly' and day in (HYSAs.month_days):
                 Prin = Prin + self.Numerical_Dictionary["CONTRIBUTION_AMOUNT"]
-                print(f"{i} {Prin}")
-            elif values["CONTRIBUTION"] == 'annually' and i == 13:
+                self.Index = self.Index + 1
+
+            elif values["CONTRIBUTION"] == 'annually' and day % 365 == 1:
                 Prin = Prin + self.Numerical_Dictionary["CONTRIBUTION_AMOUNT"]
+
             else:
                 Prin = Prin
 
-            month_New_Value = ((Prin*APY))/ 12 
-            Prin = Prin + month_New_Value
-            print(f"{i} {Prin}")
-            self.month_Array.append(round(month_New_Value, 2))
-        self.Total_OUTPUT = sum(self.month_Array)
-
-    def daily(self, Prin, contribution, month_Index):
-        for j in range(1, HYSAs.month_days[month_Index] + 1):
-            Prin = Prin + contribution
-            day = day + 1
-            if day % HYSAs.month_days[month_Index] == 0:
-                return Prin
+            if day == 365:
+                day = 0
+ 
+            day_New_Value = Prin * daily_Rate # how much is being added daily
+            Prin = Prin + day_New_Value
+            self.day_Array.append(day_New_Value)
+        self.Total_Balance = Prin
+        self.Total_OUTPUT = round(sum(self.day_Array), 2)
 
     def input_Validation(self, *args, values):
 
@@ -112,7 +106,6 @@ class HYSAs:
                 sg.popup("Input must be a integer", title = "Integer Input Error")
                 return True
         self.Numerical_Dictionary = {key: (float(values[key])) for key in args} # accessing individual keys?
-        print(self.Numerical_Dictionary)
         return False
     
     def Reset(self):
@@ -123,7 +116,7 @@ class HYSAs:
         self.window["DIFFERENCE"].update("")
         self.window["PRINCIPAL"].update("")
         self.window["APY"].update("")
-        self.window["MONTHS"].update("")
+        self.window["YEARS"].update("")
         self.window["CONTRIBUTION"].update("")
         self.window["CONTRIBUTION_AMOUNT"].update("")
         self.window["FIRST_MONTH"].update("")
